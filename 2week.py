c502db52cf5a73860bb2ae0a990f6bc498c5b62b
jjh75607 <dkrak5738@naver.com>
2020-09-08 21:12:29 +0900

    2week

--- 2week.py
+++ 2week.py
@@ -10,3 +10,10 @@ p = int(input("분자를 입력하세요 : "))
 q = int(input("분모를 입력하세요 : "))
 print("나눗셈의 몫 = ", p // q)
 print("나눗셈의 나머지 = ", p % q)
+
+n3 = int(input("제곱 할 수를 입력하세요 >> "))
+print("제곱 : ", n3 * n3)
+
+m = int(input("밑을 입력하세요 >> "))
+j = int(input("지수를 입력하세요 >> "))
+print("거듭제곱 : " , m ** j)
