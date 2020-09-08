35bb8eebf782c06d19618fc9606c4ab0166b1ebc
jjh75607 <dkrak5738@naver.com>
2020-09-08 19:32:21 +0900

    1주과제

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print("hello")
+n1 = int(input("input1  >> "))
+n2 = int(input("input 2 >> "))
+
+print(n1, "+", n2, "=", n1 + n2)
+print(n1, "-", n2, "=", n1 - n2)
+print(n1, "*", n2, "=", n1 * n2)
+print(n1, "/", n2, "=", n1 / n2)
+
+p = int(input("분자를 입력하세요 : "))
+q = int(input("분모를 입력하세요 : "))
+print("나눗셈의 몫 = ", p // q)
+print("나눗셈의 나머지 = ", p % q)
