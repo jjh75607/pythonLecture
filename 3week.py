886711dff37b51a1001529020dbc2c85e9359ce6
jjh75607 <dkrak5738@naver.com>
2020-09-15 22:36:37 +0900

    3week

---
+++ 3week.py
@@ -0,0 +1,28 @@
+# 다이아몬드
+print('    *')
+print("   ***")
+print("  *****")
+print(" *******")
+print("*********")
+print(" *******")
+print("  *****")
+print("   ***")
+print('    *')
+# 피라미드
+print("   ***")
+print("  *****")
+print(" *******")
+print("*********")
+
+print("\n")
+
+# 나비 모양
+print("*         *")
+print("**      **")
+print("***   ***")
+print("**** ****")
+print("*********")
+print("**** ****")
+print("***   ***")
+print("**      **")
+print("*         *")
