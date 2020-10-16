c5580b4d1e1419650bf27fc6f868e0978bd76a37
jjh75607 <dkrak5738@naver.com>
2020-10-16 19:32:15 +0900

    7week

---
+++ 7week.py
@@ -0,0 +1,10 @@
+def hanoi(n, start, mid, end):
+    if n == 1:
+        print(start, "->", end)
+        return 1
+    hanoi(n - 1, start, mid, end)
+    print(start, "->", end)
+    hanoi(n-1, mid, end, start)
+
+
+hanoi(3, 1, 3, 2)
